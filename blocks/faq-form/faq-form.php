0103ca95e57c98c8cfeec77ca262bf653854df81
Creastv <piotr@roial.pl>
2023-02-28 08:53:52 +0100

    faq

--- blocks/faq-form/faq-form.php
+++ blocks/faq-form/faq-form.php
@@ -5,7 +5,7 @@ $form = get_field( 'form' );
 <div class="b-faq-form">
     <div class="b-faq-form__wraper">
         <div class="col">
-            <?php echo do_shortcode('[contact-form-7 id="780" title="Formularz 1"]')?>
+            <?php echo do_shortcode('[contact-form-7 id="780" title="FAQ"]')?>
         </div>
     </div>
 </div>
