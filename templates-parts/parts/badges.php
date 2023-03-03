249cd1eb41142b0493689f6e4b16b5e8106f555b
Creastv <piotr@roial.pl>
2023-03-03 14:19:29 +0100

    fixing

--- templates-parts/parts/badges.php
+++ templates-parts/parts/badges.php
@@ -1,6 +1,6 @@
 <div class="badges">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/gwarancja-usmiechu.webp" alt="Gwarancja Uśmiechu - Hello Smile" width="80px" height="80px">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/polski-produkt.webp" alt="Polski produkt - Hello Smile" width="80px" height="80px">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/ISO-9001-2015.webp" alt="ISO 2015 - Hello Smile" width="80px" height="80px">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/ISO-13485-2016.webp" alt="ISO 2016 - Hello Smile" width="80px" height="80px">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/gwarancja-usmiechu-120.png" alt="Gwarancja Uśmiechu - Hello Smile" width="120px" height="120px">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/polski-produkt-120.png" alt="Polski produkt - Hello Smile" width="120px" height="120px">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/ISO-9001-2015-120.png" alt="ISO 2015 - Hello Smile" width="120px" height="120px">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/img/ISO-13485-2016-120.png" alt="ISO 2016 - Hello Smile" width="120px" height="120px">
 </div>
