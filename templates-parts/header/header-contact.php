e7cce7633b077000c586f636c50a58adfb8f067e
Creastv <piotr@roial.pl>
2023-03-07 13:45:57 +0100

    all done

--- templates-parts/header/header-contact.php
+++ templates-parts/header/header-contact.php
@@ -1,3 +1,13 @@
+<?php 
+$link = get_field( 'link_', 'options' );
+if( $link ) {
+    $link_url = $link['url'];
+    $link_title = $link['title'];
+    $link_target = $link['target'] ? $link['target'] : '_self';
+}
+?>
 <div class="navbar__cta">
-    <a href="#" class="btn">Zamów rozmowę</a>
+    <?php if($link) {  ?>
+    <a class="btn" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+    <?php } ?>
 </div>
