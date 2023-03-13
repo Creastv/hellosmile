25b0f7a442b9dcf51a5b23ae8fb469c8ae433e24
Creastv <piotr@roial.pl>
2023-03-13 15:57:38 +0100

    domain-ver

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
+    <meta name="facebook-domain-verification" content="xba2y940sv5pu64jxixfce3k3hz52j" />
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
