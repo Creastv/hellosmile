f84e9b6f57b60a1ec1c71073aeb245e75b867a79
Creastv <piotr@roial.pl>
2023-04-19 10:38:35 +0200

    update

--- footer.php
+++ footer.php
@@ -17,6 +17,8 @@
         <path id="Icon_material-arrow-upward" data-name="Icon material-arrow-upward" d="M6,18l2.115,2.115,8.385-8.37V30h3V11.745l8.37,8.385L30,18,18,6Z" transform="translate(-6 -6)" />
     </svg>
 </span>
+<div class="tr-widget" data-id="2636" data-view="flash" data-lang=""><a href="https://trust.reviews/" class="trcr" target="_blank">Powered by <span>Trust.Reviews</span></a></div>
+<script type="text/javascript" src="https://cdn.trust.reviews/widget/embed.min.js" defer></script>
 
 <?php wp_footer(); ?>
 </body>
