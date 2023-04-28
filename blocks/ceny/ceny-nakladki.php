f046dff5479d07c1d45bb7a02ccdf02a7b697cca
Creastv <piotr@roial.pl>
2023-04-28 15:39:41 +0200

    cupon

--- blocks/ceny/ceny-nakladki.php
+++ blocks/ceny/ceny-nakladki.php
@@ -5,8 +5,7 @@ $nakladki = get_field( 'nakladki' );
 <div class="b-ceny-nakladek">
     <div class="b-ceny-nakladek__wraper">
         <?php foreach($nakladki as $item) {?>
-        <div class="b-ceny-nakladek__item">
-            <div class="nazwa">
+           <?php echo $item['link'] ? ' <a class="b-ceny-nakladek__item" href=" ' . $item['link'] . ' " >' : ' <div class="b-ceny-nakladek__item">'; ?>         <div class="nazwa">
                 <?php if($item['nazwa']) { ?>
                 <h3><?php echo $item['nazwa']; ?></h3>
                 <?php } ?>
@@ -29,7 +28,7 @@ $nakladki = get_field( 'nakladki' );
                     <?php } ?>
                 </div>
             </div>
-        </div>
+           <?php echo $item['link'] ? '</a>' : '</div>'; ?>
         <?php } ?>
     </div>
 </div>
